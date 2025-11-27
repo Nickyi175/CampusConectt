7a25c451192bfc58bde4a246261679ba9cfb331d
Nickyi175 <tome.andrea1378@gmail.com>
2025-11-26 22:06:57 -0600

    app

--- campusConect-api/routes/users.js
+++ campusConect-api/routes/users.js
@@ -1,3 +1,94 @@
+/**
+ * @swagger
+ * components:
+ *  schemas:
+ *      usuarios:
+ *          type: object
+ *          required:
+ *            - id
+ *            - nombre
+ *            - correo
+ *            - password
+ *            - carrera
+ *            - foto
+ *          properties:
+ *            id:
+ *              type: integer
+ *              description: The auto-generated id of the usuarios
+ *            nombre:
+ *              type: string
+ *              description: Nombre de usuarios
+ *            correo:
+ *              type: string
+ *              description: Correo de usuarios
+ *            password:
+ *              type: string
+ *              description: Contraseña segura del usuario
+ *            carrera:
+ *              type: string
+ *              description: Carrera de usuarios
+ *            foto:
+ *              type: string
+ *              description: Foto del usuario
+ * 
+ * 
+ */
+/**
+ * @swagger
+ * tags:
+ *   name: usuarios
+ *   description: Endpoints del módulo de usuarios
+ *
+ * /users/new:
+ *   post:
+ *     summary: Crear un nuevo usuario
+ *     tags: [usuarios]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/x-www-form-urlencoded:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               nombre:
+ *                 type: string
+ *                 description: Nombre de Usuario
+ *               correo:
+ *                 type: string
+ *                 description: Correo de Usuario
+ *               password:
+ *                 type: string
+ *                 description: Clave encriptada de 8 a 15 caracteres
+ *               carrera:
+ *                 type: string
+ *                 description: Carrera que estudia el usuario
+ *               foto:
+ *                 type: string
+ *                 description: Imagen de perfil del usuario
+ *             required:
+ *               - nombre
+ *               - correo
+ *               - password
+ *               - carrera
+ *               - foto
+ *
+ *     responses:
+ *       201:
+ *         description: Usuario creado con éxito.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                 data:
+ *                   type: array
+ *                   items:
+ *                     $ref: '#/components/schemas/usuarios'
+ */
+
+
 //aqui manejo rutas 
 
 var router = require('express').Router();//este me ayuda a manejar las rutas 
